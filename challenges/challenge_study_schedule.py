453278bf1d1d35c2ac4f6d3a3389be3082fb7c8a
Williamns <williamnstolb@gmail.com>
2022-07-04 12:26:28 -0300

    Requisito 1: Verificação dos tipos de entrada e saída sendo inteiros para prosseguir

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,6 +7,9 @@ def study_schedule(permanence_period, target_time):
     
     counter = 0
     for check_in, check_out in permanence_period:
-        if check_in <= target_time <= check_out:
-            counter += 1
+        if not (isinstance(check_in, int) and isinstance(check_out, int)):
+            return None
+        else: 
+            if check_in <= target_time <= check_out:
+                counter += 1
     return counter
