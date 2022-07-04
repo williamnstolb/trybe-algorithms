b27e21279b42033fcd47f1ac19f8a10f4bf17512
Williamns <williamnstolb@gmail.com>
2022-07-04 16:19:24 -0300

    Requisito 4: Falta verificação se é string

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,10 +1,12 @@
 def find_duplicate(nums):
-    original_list = nums.sort()
-    if isinstance(nums, str) or len(nums) == 0 or len(nums) == 1 or nums[0] < 0:
-        return False    
-
-    duplicate_number_list = list()
-    for i in range(len(original_list)):
-        if original_list[i] == original_list[i+1]:
-            duplicate_number_list.append(original_list[i])
-    return duplicate_number_list
+    if len(nums) == 0 or len(nums) == 1 or nums[0] <= 0:
+        return False
+    copy_list = list()
+    for number in nums:
+        if type(number) != int:
+            return False
+        if number not in copy_list:
+            copy_list.append(number)
+        else:
+            return number
+    return False
