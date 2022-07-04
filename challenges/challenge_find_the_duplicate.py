3da4584cb76a3541bdb685edfcc6451d933991ae
Williamns <williamnstolb@gmail.com>
2022-07-04 15:28:26 -0300

    Requisito 4: Verificação se a lista não está vazia

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if isinstance(nums, list) or len(nums) == 0:
+        return None
+    original_list = nums.sort()
+    duplicate_number_list = list()
+    for i in range(len(original_list)):
+        if original_list[i] == original_list[i+1]:
+            duplicate_number_list.append(original_list[i])
+    return duplicate_number_list
