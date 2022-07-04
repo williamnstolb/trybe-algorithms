804e0569708f1d122d551f32770dad1209a6b994
Williamns <williamnstolb@gmail.com>
2022-07-04 15:17:00 -0300

    Requisito 3: Passando nos testes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_word = list(first_string.lower())
+    second_word = list(second_string.lower())
+    if len(first_word) != len(second_word):
+        return False
+    for letter in first_word:
+        if letter in second_word:
+            second_word.remove(letter)
+        else:
+            return False
+    return True
