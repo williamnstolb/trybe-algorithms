d610e4416d4eee026cf47913d17c3c367127f8d7
Williamns <williamnstolb@gmail.com>
2022-07-04 16:21:24 -0300

    Requisito 4: Passando em todos os testes

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,9 @@
 def find_duplicate(nums):
-    if len(nums) == 0 or len(nums) == 1 or nums[0] <= 0:
+    if len(nums) == 0 or len(nums) == 1:
         return False
     copy_list = list()
     for number in nums:
-        if type(number) != int:
+        if type(number) != int or number <= 0:
             return False
         if number not in copy_list:
             copy_list.append(number)
