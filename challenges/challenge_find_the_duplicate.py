c55d8adecad1b709d8538b544ec3d88638130e84
Williamns <williamnstolb@gmail.com>
2022-07-04 15:32:53 -0300

    Requisito 4: Verificação se a lista não tem número negativo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,7 +1,8 @@
 def find_duplicate(nums):
-    if isinstance(nums, str) or len(nums) == 0 or len(nums) == 1:
-        return False
     original_list = nums.sort()
+    if isinstance(nums, str) or len(nums) == 0 or len(nums) == 1 or nums[0] < 0:
+        return False    
+
     duplicate_number_list = list()
     for i in range(len(original_list)):
         if original_list[i] == original_list[i+1]:
