dbfabcc8b48927e64316e424cf4312d74ed4b47b
Williamns <williamnstolb@gmail.com>
2022-07-04 13:22:09 -0300

    FIX linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
-        return None    
+        return None
     counter = 0
     for check_in, check_out in permanence_period:
         if not (isinstance(check_in, int) and isinstance(check_out, int)):
