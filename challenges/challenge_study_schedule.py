5cba240eb2db78e7c2598ce4dc65fd66a96539e8
Williamns <williamnstolb@gmail.com>
2022-07-04 12:14:12 -0300

    Requisito 1: Não está passando em todos os testes

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
+from urllib.parse import _NetlocResultMixinStr
+
+
 def study_schedule(permanence_period, target_time):
-    ...
+    if target_time is None:
+        return None
+    
+    counter = 0
+    for check_in, check_out in permanence_period:
+        if check_in <= target_time <= check_out:
+            counter += 1
+    return counter
