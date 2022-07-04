420d4b99b6edf6ce99319f37be47d06a6d018864
Williamns <williamnstolb@gmail.com>
2022-07-04 12:40:50 -0300

    Requisito 2: Não está passando em todos os testes ainda

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,11 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
-        return None
-    
+        return None    
     counter = 0
     for check_in, check_out in permanence_period:
         if not (isinstance(check_in, int) and isinstance(check_out, int)):
             return None
-        else: 
+        else:
             if check_in <= target_time <= check_out:
                 counter += 1
     return counter
